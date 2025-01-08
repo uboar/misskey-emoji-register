1cfe86cc57b902aa660c8837549f56ce5203e479
FineArchs <cicada1003@gmail.com>
2025-01-08 09:19:50 +0900

    add editor

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,12 @@
   import { onMount, tick } from "svelte";
   import EmojiRegister from "./components/EmojiRegister.svelte";
   import GetNote from "./components/GetNote.svelte";
+  import EditNote from "./components/EditNote.svelte";
 
-  import { emojis, getCookie } from "./lib/store";
+  import { emojis, getCookie, note } from "./lib/store";
   import { load } from "./lib/ffmpeg";
 
-  let selectedTab = 0;
+  let selectedTab: null | 'getnote' | 'editnote' | number = 'getnote';
 
   getCookie();
 
@@ -14,10 +15,10 @@
     await load();
   });
 
-  const tabSelect = async (tabNum: number) => {
-    selectedTab = -1;
+  const tabSelect = async (tabId: typeof selectedTab) => {
+    selectedTab = null;
     await tick();
-    selectedTab = tabNum;
+    selectedTab = tabId;
   };
 </script>
 
@@ -32,14 +33,21 @@
     <div role="tablist" class="tabs tabs-bordered bg-base-300 shadow overflow-auto">
       <button
         role="tab"
-        class="tab {selectedTab === 0 ? 'tab-active' : ''} min-w-max"
-        onclick={() => tabSelect(0)}>ノート取得</button
+        class="tab {selectedTab === 'getnote' ? 'tab-active' : ''} min-w-max"
+        onclick={() => tabSelect('getnote')}>ノート取得</button
       >
+      {#if $note != null}
+        <button
+          role="tab"
+          class="tab {selectedTab === 'editnote' ? 'tab-active' : ''} min-w-max"
+          onclick={() => tabSelect('editnote')}>申請の編集</button
+        >
+      {/if}
       {#each $emojis as emoji, index}
         <button
           role="tab"
-          class="tab {selectedTab === 1 + index ? 'tab-active' : ''} overflow-hidden"
-          onclick={() => tabSelect(1 + index)}
+          class="tab {selectedTab === index ? 'tab-active' : ''} overflow-hidden"
+          onclick={() => tabSelect(index)}
 	>
 	  <div class="min-w-4 flex flex-row">
             <img class="w-4 object-contain" src={emoji.file.url} />
@@ -50,12 +58,14 @@
     </div>
 
     <div class="p-4">
-      {#if selectedTab === 0}
-        <GetNote></GetNote>
-      {:else if selectedTab === -1}
+      {#if selectedTab == null}
         <div></div>
+      {:else if selectedTab === 'getnote'}
+        <GetNote></GetNote>
+      {:else if selectedTab === 'editnote'}
+        <EditNote></EditNote>
       {:else}
-        <EmojiRegister bind:emoji={$emojis[selectedTab - 1]}></EmojiRegister>
+        <EmojiRegister bind:emoji={$emojis[selectedTab]}></EmojiRegister>
       {/if}
     </div>
   </div>
---
+++ src/components/EditNote.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import {
+    note,
+    emojis,
+  } from "../lib/store";
+  import { splitEmojis } from "../lib/misskey";
+
+  let noteText = $note.text;
+  $: {
+    $note.text = noteText;
+  }
+    
+  async function updateNote() {
+    const emojisData = splitEmojis($note);
+    emojis.set(emojisData);
+  };
+</script>
+
+<div>
+  <textarea bind:value={noteText}></textarea>
+  <button class="btn btn-primary" onclick={updateNote}>更新</button>
+</div>
