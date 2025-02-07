0f8023c3353ad0fa1c4d105850a9c858f5c3b960
FineArchs <cicada1003@gmail.com>
2025-02-07 18:15:48 +0900

    Typescriptのエラーを修正

--- src/lib/miauth.svelte.ts
+++ src/lib/miauth.svelte.ts
@@ -2,9 +2,10 @@ import { api } from "misskey-js";
 import { v4 as uuid } from "uuid";
 import { serverUrl, accessToken } from "./store";
 import { get } from "svelte/store";
+import type { MiauthGenTokenResponse } from "misskey-js/entities.js";
 
 export class MiAuth {
-  private sessionId = $state(null);
+  private sessionId = $state(null as null | string);
   constructor() {}
 
   readonly isTokenReady = $derived(this.sessionId != null);
@@ -22,7 +23,7 @@ export class MiAuth {
   async requestToken(): Promise<void> {
     if (this.sessionId == null) throw new Error("token is not ready");
     const client = new api.APIClient({ origin: get(serverUrl) });
-    const { token } = await client.request(`miauth/${this.sessionId}/check`);
+    const { token } = await client.request(`miauth/${this.sessionId}/check` as any, {}) as any as MiauthGenTokenResponse;
     this.sessionId = null;
     accessToken.set(token);
   }
